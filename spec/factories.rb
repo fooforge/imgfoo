dfd12261c67df4c6812f0738238c57301838b845
Mike Adolphs <mike@fooforge.com>
2011-12-21 17:09:31 +0100

    Added factory girl gem for testing, began building the user's page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Mike Adolphs"
+  user.email                  "mike@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
