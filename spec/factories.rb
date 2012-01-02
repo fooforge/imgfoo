5f161a21ccca9810161c8be635cea15ef6d1e1f0
Mike Adolphs <mike@fooforge.com>
2012-01-02 22:42:09 +0100

    Added user index with pagination, used faker gem for mass-generation of dummy users, added admin functionality for deleting users

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,11 @@ Factory.define :user do |user|
   user.password               "Aih1xiv/"
   user.password_confirmation  "Aih1xiv/"
 end
+
+Factory.sequence :name do |n|
+  "Person #{n}"
+end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
