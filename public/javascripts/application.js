c7c608772ccf9ab7afb032299c6112c114da346a
Mike Adolphs <mike@fooforge.com>
2012-01-15 02:15:48 +0100

    Removed Blueprint CSS to KISS. Added TopUp for image viewing, altered stylesheet a lot

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+TopUp.Host = "http://127.0.0.1/";
+TopUp.images_path = "/images/top_up/";    
+TopUp.addPresets({
+  ".top_up": {
+    layout: 'flatlook',
+    effect: 'appear',
+    shaded: 1,
+    overlayClose: 1,
+    group: "images"
+  },
+});
